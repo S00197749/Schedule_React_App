cef87005b5b2cd45fdcae934056b1b0302b81a1c
Luke Sweeney <lukesweeney00@gmail.com>
2023-04-24 12:25:49 +0100

    Added GetUserSchedule functionality

--- src/components/Schedule/UserSchedule.js
+++ src/components/Schedule/UserSchedule.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Inject,ScheduleComponent,Day,Week,Month,ViewsDirective, ViewDirective } from "@syncfusion/ej2-react-schedule";
 
 function GroupSchedule(props) {
-    const data = {
-        dataSource: [{
-            Subject: "Meeting",
-            EndTime: new Date(2023, 3, 25, 16, 30),
-            StartTime: new Date(2023, 3, 25, 14, 30)
-        },
-        {
-            Subject: "Meeting 2",
-            EndTime: new Date(2023, 3, 24, 20, 30),
-            StartTime: new Date(2023, 3, 24, 18, 30)
-        }]
-    }
+    const [userSchedule, setUserSchedule] = useState([]);
+
+    const url = "https://schedule-functions.azurewebsites.net/api/GetUserSchedule?code=5ZyIAL7d-IMLSqAo4J2vwkwK5IMYmZoxgOZTpuH9WeYiAzFurM3cbQ==";
+
+    const u = props.user_Id;
+
+    const fetchUserScheduleData = async () => {
+		const userScheduleResult = await fetch(url + "&u=" + u)
+		const userScheduleJsonResult = await userScheduleResult.json();
+
+		setUserSchedule(userScheduleJsonResult)
+	}
+
+    const data = userSchedule;
+
+    React.useEffect(() => {
+
+        fetchUserScheduleData();
+      },[]);
 
     function editorTemplate(timeSlotProp) {
         return ((timeSlotProp !== undefined) ? <div>{timeSlotProp.Subject }</div> : <div>Edit Event</div>);
@@ -33,7 +40,16 @@ function GroupSchedule(props) {
                 </div>
             </div>
             <div class="row mb-2 mb-xl-3">
-                <ScheduleComponent height={"900px"} editorTemplate={editorTemplate.bind(this)} eventSettings={data} >
+                <ScheduleComponent height={"900px"} editorTemplate={editorTemplate.bind(this)} 
+                eventSettings={{
+                    dataSource: data,
+                    fields:{
+                        id: 'timeslot_Id',
+                        subject: { default: 'Available' },
+                        startTime: { title: 'From', name: 'startTime' },
+                        endTime: { title: 'To', name: 'endTime' }
+                    }
+                }}>
                     <ViewsDirective>
                         <ViewDirective option='Day' />
                         <ViewDirective option='Week' />
